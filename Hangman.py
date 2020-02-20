d86830262ebffd84b3320665edacc2234cf73ea9
Pheewuh <45989735+Pheewuh@users.noreply.github.com>
2020-02-20 18:10:08 +0600

    Add files via upload

---
+++ Hangman.py
@@ -0,0 +1,36 @@
+hangman=[' ','---------','    ¦   ','    O    ','   /¦/   ','    ¦    ','   /¦/   ']
+word=list(input('Input your word:'))
+worddupe=word[::1]
+mistakes=1
+displayword=[]
+for i in word:
+	if i in 'aeiou':
+		displayword.append('_')
+	else:
+		displayword.append('-')
+def guessing():
+	print(displayword)
+	global mistakes
+	global correctonce
+	guess=input('Guess:')
+	def guesswork():
+		global mistakes
+		global correctonce
+		global falseonce
+		if guess in word:
+			print('Correct')
+			displayword[word.index(guess)]=guess
+			word[word.index(guess)]=0
+			del worddupe[0]
+			correctonce=True
+		elif guess not in word and correctonce==False and falseonce==False:
+			mistakes+=1
+			print('Wrong')
+			for i in range(mistakes):
+				print(hangman[i]+'\n')
+			falseonce=True	
+	[guesswork() for i in range(100)]
+while mistakes<7 and len(worddupe)>0:
+	guessing()
+	correctonce=False
+	falseonce=False
